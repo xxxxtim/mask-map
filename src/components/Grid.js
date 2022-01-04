572c043b2f9d6ad913498b0601c560257c4d7cea
Tim <xxxxtim@hotmail.com>
2022-01-04 21:49:11 +0800

    feat: add btsConnection json file

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,32 +1,35 @@
-import { Layout, Menu } from 'antd';
-import Map from '../components/Map'
-import Menus from './Menus'
+import { Layout, Menu } from "antd";
+import Map from "../components/Map";
+import Menus from "./Menus";
 
 const { Header, Content, Footer, Sider } = Layout;
-
+//  this is apple
 const Grid = () => {
-    return (<Layout>
-        <Sider
-            style={{
-                overflow: 'auto',
-                height: '100vh',
-                position: 'fixed',
-                left: 0,
-            }}
-        >
-            <div className="logo" />
-            <Menus />
-        </Sider>
-        <Layout style={{ marginLeft: 200 }}>
-            <Header style={{ padding: 0, background: '#fff' }} />
-            <Content style={{ margin: '24px 16px 0' }}>
-                <div style={{ padding: 24, minHeight: 360, background: '#fff' }}>
-                    <Map />
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
-    </Layout>)
-}
-export default Grid
-
+  return (
+    <Layout>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menus />
+      </Sider>
+      <Layout style={{ marginLeft: 200 }}>
+        <Header style={{ padding: 0, background: "#fff" }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div style={{ padding: 24, minHeight: 360, background: "#fff" }}>
+            <Map />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default Grid;
